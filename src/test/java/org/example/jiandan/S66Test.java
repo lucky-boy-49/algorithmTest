594dc25b0e7c8c6750efa1b15795bddfcd1e39e5
HeJia <jiahe634@gmail.com>
2024-10-09 11:21:35 +0800

    66. 加一

---
+++ src/test/java/org/example/jiandan/S66Test.java
@@ -0,0 +1,14 @@
+package org.example.jiandan;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class S66Test {
+
+    @Test
+    void plusOne() {
+        S66 s66 = new S66();
+        System.out.println(Arrays.toString(s66.plusOne(new int[]{9})));
+    }
+}
