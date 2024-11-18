e6398b268df9cfa6307b4c1941d15a6d8975b695
HeJia <jiahe634@gmail.com>
2024-11-18 17:14:30 +0800

    123. 买卖股票的最佳时机 III

---
+++ src/test/java/org/example/kunnan/S123Test.java
@@ -0,0 +1,15 @@
+package org.example.kunnan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class S123Test {
+
+    @Test
+    void maxProfit() {
+        S123 s123 = new S123();
+        assertEquals(6, s123.maxProfit(new int[]{3,3,5,0,0,3,1,4}));
+    }
+
+}
