719f24793cf798ba959e4a3ff451b6561dbdd4ee
HeJia <jiahe634@gmail.com>
2024-12-12 09:22:39 +0800

    学习junit5

--- src/test/java/org/example/jiandan/S703Test.java
+++ src/test/java/org/example/jiandan/S703Test.java
@@ -1,29 +1,23 @@
 package org.example.jiandan;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class S703Test {
+    static S703 s703;
 
-    @Test
-    void test() {
-        S703 s703 = new S703(3, new int[]{4, 5, 8, 2});
-        assertEquals(4, s703.add(3));
-        assertEquals(5, s703.add(5));
-        assertEquals(5, s703.add(10));
-        assertEquals(8, s703.add(9));
-        assertEquals(8, s703.add(4));
+    @BeforeAll
+    static void beforeAll() {
+        s703 = new S703(3, new int[]{4, 5, 8, 2});
     }
 
-    @Test
-    void test2() {
-        S703 s703 = new S703(1, new int[]{});
-        assertEquals(-3, s703.add(-3));
-        assertEquals(-2, s703.add(-2));
-        assertEquals(-2, s703.add(-4));
-        assertEquals(0, s703.add(0));
-        assertEquals(4, s703.add(4));
+    @ParameterizedTest
+    @CsvSource({"4, 3", "5, 5", "5, 10", "8, 9", "8, 4"})
+    void test(int expected, int actual) {
+        assertEquals(expected, s703.add(actual));
     }
 
 }
